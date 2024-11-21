84034e1beb15c3145c48dd009dbc72d796e6243c
aloycwl <aloycwl@gmail.com>
2024-11-21 10:46:33 +0800

    Reduced gas

--- contract/Deploy.sol
+++ contract/Deploy.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.28;
 
 import {Reward} from "./Reward.sol";
-import {IERC20} from "https://github.com/magape-io/contracts/blob/main/Interface/IERC20.sol";
 import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
 import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
 
@@ -10,19 +9,15 @@ contract Deploy is Hashes {
     address public RewardAddress;
 
     constructor() {
-        (bytes32[] memory arr1, bytes32[] memory arr2) = (
-            new bytes32[](2),
-            new bytes32[](2)
-        );
+        RewardAddress = address(new Upgrade(address(new Reward())));
 
-        (arr1[0], arr1[1], arr2[0], arr2[1], RewardAddress) = (
-            APP,
-            OWO,
-            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505, // Signer
-            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505, // Owner
-            address(new Upgrade(address(new Reward())))
+        Upgrade(payable(RewardAddress)).mem(
+            APP, // Signer
+            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505
         );
-
-        Upgrade(payable(RewardAddress)).mem(arr1, arr2);
+        Upgrade(payable(RewardAddress)).mem(
+            OWO, // Owner
+            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505
+        );
     }
 }
