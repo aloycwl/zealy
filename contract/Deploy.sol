f9c6a5043ac9acaf4995d5c8cd0517d11458ee8e
aloycwl <aloycwl@gmail.com>
2024-11-21 10:37:10 +0800

    Removed assembly codes

--- contract/Deploy.sol
+++ contract/Deploy.sol
@@ -2,42 +2,27 @@
 pragma solidity 0.8.28;
 
 import {Reward} from "./Reward.sol";
-import {USDTMock} from "https://github.com/magape-io/contracts/blob/main/tests/USDTMock.sol";
 import {IERC20} from "https://github.com/magape-io/contracts/blob/main/Interface/IERC20.sol";
 import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
 import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
 
 contract Deploy is Hashes {
+    address public RewardAddress;
+
     constructor() {
-        address r = address(new Upgrade(address(new Reward())));
+        (bytes32[] memory arr1, bytes32[] memory arr2) = (
+            new bytes32[](2),
+            new bytes32[](2)
+        );
 
-        /***  For testing use ****/
-        address usdt = address(new USDTMock());
-        IERC20(usdt).transfer(r, 1e22);
-        /***  Comment for deployment ****/
+        (arr1[0], arr1[1], arr2[0], arr2[1], RewardAddress) = (
+            APP,
+            OWO,
+            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505, // Signer
+            0x000000000000000000000000A34357486224151dDfDB291E13194995c22Df505, // Owner
+            address(new Upgrade(address(new Reward())))
+        );
 
-        assembly {
-            sstore(0x01, r)
-            // Upgrade(mag).mem(APP, adr);
-            mstore(
-                0x80,
-                0xb88bab2900000000000000000000000000000000000000000000000000000000
-            )
-            // r.signer
-            mstore(0x84, APP)
-            mstore(0xa4, 0xA34357486224151dDfDB291E13194995c22Df505)
-            pop(call(gas(), r, 0x00, 0x80, 0x44, 0x00, 0x00))
-            // r.owner
-            mstore(0x84, OWO)
-            mstore(0xa4, 0xA34357486224151dDfDB291E13194995c22Df505)
-            pop(call(gas(), r, 0x00, 0x80, 0x44, 0x00, 0x00))
-        }
-    }
-
-    function RewardAddress() external view returns (address) {
-        assembly {
-            mstore(0x00, sload(0x01))
-            return(0x00, 0x20)
-        }
+        Upgrade(payable(RewardAddress)).mem(arr1, arr2);
     }
 }
