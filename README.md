4f3f1d470774731ca501100a2ea862cc102469ea
aloycwl <aloycwl@gmail.com>
2024-11-21 09:25:32 +0800

    Update

---
+++ README.md
@@ -0,0 +1,36 @@
+# Deployed Reward Contracts
+
+
+
+| | | | |
+|---|---|---|---|
+| [BSC Testnet](#bsc-testnet) | [Sepolia Testnet](#sepolia-testnet) |  |  |
+
+
+## [BSC Testnet]
+
+**Reward**
+```
+0x995247107540f4d3378e0E8500A8e19f264e2C6a 
+```
+***Signer & Onwer***
+```
+0xA34357486224151dDfDB291E13194995c22Df505
+```
+[Back to top](#deployed-Reward-contracts)
+
+---
+
+## [Sepolia Testnet]
+
+**Reward**
+```
+0x28E859ba5354630fcE3D80872e2061B3F7Acc209
+```
+***Signer & Onwer***
+```
+0xA34357486224151dDfDB291E13194995c22Df505
+```
+[Back to top](#deployed-Reward-contracts)
+
+---
---
+++ contract/Reward.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.28;
+
+import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
+import {Ownable} from "https://github.com/magape-io/contracts/blob/main/Util/Ownable.sol";
+import {IERC20} from "https://github.com/magape-io/contracts/blob/main/Interface/IERC20.sol";
+import {ECDSA} from "https://github.com/magape-io/contracts/blob/main/Util/ECDSA.sol";
+
+contract Reward is Ownable, ECDSA {
+    function reward(bytes memory c) external {
+        uint256 a;
+        uint256 b;
+        uint256 n;
+        address t;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        assembly {
+            n := mload(add(c, 0x20))
+            t := n
+            a := mload(add(c, 0x40))
+            b := mload(add(c, 0x60))
+            v := mload(add(c, 0x80))
+            r := mload(add(c, 0xa0))
+            s := mload(add(c, 0xc0))
+        }
+
+        isVRS(b, a, n, v, r, s);
+
+        IERC20(t).transfer(msg.sender, a);
+    }
+
+    function removeLiquid(uint256 _amount, address _tokenaddress)
+        external
+        onlyOwner
+    {
+        IERC20(_tokenaddress).transfer(msg.sender, _amount);
+    }
+}
