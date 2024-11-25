fe869ae64a704664528f3cbc019aa2bac497b847
aloycwl <aloycwl@gmail.com>
2024-11-25 11:09:55 +0800

    streamlined

--- contract/Deploy.sol
+++ contract/Deploy.sol
@@ -5,14 +5,13 @@ import {Reward} from "./Reward.sol";
 import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
 import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
 
+/*** FOR TESTING ONLY ***/
 import {USDTMock} from "https://github.com/magape-io/contracts/blob/main/tests/USDTMock.sol";
 
 contract Deploy is Hashes {
-    address public RewardAddress;
+    address payable public adr;
 
     constructor() payable {
-        RewardAddress = address(new Upgrade(address(new Reward())));
-
         bytes32 owner;
         bytes32 signer;
 
@@ -21,10 +20,10 @@ contract Deploy is Hashes {
             signer := 0xA34357486224151dDfDB291E13194995c22Df505
         }
 
-        Upgrade(payable(RewardAddress)).mem(APP, signer);
-        Upgrade(payable(RewardAddress)).mem(OWO, owner);
+        Upgrade(adr = payable(address(new Upgrade(address(new Reward()))))).mem(APP, signer);
+        Upgrade(adr).mem(OWO, owner);
 
-        address _u = address(new USDTMock());
-        USDTMock(_u).transfer(RewardAddress, 0xd3c21bcecceda1000000);
+        /*** FOR TESTING ONLY ***/
+        USDTMock(address(new USDTMock())).transfer(adr, 0xd3c21bcecceda1000000);
     }
 }
