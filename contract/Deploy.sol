11a553aa185f608f2a5c240966c976f02769e59f
aloycwl <aloycwl@gmail.com>
2024-11-25 12:49:27 +0800

    Remove mock usdt

--- contract/Deploy.sol
+++ contract/Deploy.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.28;
 import {Reward} from "./Reward.sol";
 import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
 import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
-import {USDTMock} from "https://github.com/magape-io/contracts/blob/main/tests/USDTMock.sol";
 
 contract Deploy is Hashes {
     address payable public adr;
@@ -23,6 +22,5 @@ contract Deploy is Hashes {
             signer
         );
         Upgrade(adr).mem(OWO, owner);
-        USDTMock(address(new USDTMock())).transfer(adr, 0xd3c21bcecceda1000000);
     }
 }
