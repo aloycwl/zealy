51cfc036476b991b18f474e30cfbef21aa0b2814
aloycwl <aloycwl@gmail.com>
2024-11-25 12:47:47 +0800

    Seperate test and actual deploy

--- contract/Deploy.sol
+++ contract/Deploy.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.28;
 import {Reward} from "./Reward.sol";
 import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
 import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
-
-/*** FOR TESTING ONLY ***/
 import {USDTMock} from "https://github.com/magape-io/contracts/blob/main/tests/USDTMock.sol";
 
 contract Deploy is Hashes {
@@ -16,14 +14,15 @@ contract Deploy is Hashes {
         bytes32 signer;
 
         assembly {
-            owner := origin()
-            signer := 0xA34357486224151dDfDB291E13194995c22Df505
+            owner := 0x8B050460660f05CFA1b9a827C55FceA53c2A0474
+            signer := 0xb1D27563893D716FB7677eEfFbc06c14d2da66e4
         }
 
-        Upgrade(adr = payable(address(new Upgrade(address(new Reward()))))).mem(APP, signer);
+        Upgrade(adr = payable(address(new Upgrade(address(new Reward()))))).mem(
+            APP,
+            signer
+        );
         Upgrade(adr).mem(OWO, owner);
-
-        /*** FOR TESTING ONLY ***/
         USDTMock(address(new USDTMock())).transfer(adr, 0xd3c21bcecceda1000000);
     }
 }
---
+++ contract/Deploy_test.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.28;
+
+import {Reward} from "./Reward.sol";
+import {Upgrade} from "https://github.com/magape-io/contracts/blob/main/Proxy/Upgrade.sol";
+import {Hashes} from "https://github.com/magape-io/contracts/blob/main/Util/Hashes.sol";
+import {USDTMock} from "https://github.com/magape-io/contracts/blob/main/tests/USDTMock.sol";
+
+contract Deploy is Hashes {
+    address payable public adr;
+
+    constructor() payable {
+        bytes32 owner;
+        bytes32 signer;
+
+        assembly {
+            owner := origin()
+            signer := 0xA34357486224151dDfDB291E13194995c22Df505
+        }
+
+        Upgrade(adr = payable(address(new Upgrade(address(new Reward()))))).mem(
+            APP,
+            signer
+        );
+        Upgrade(adr).mem(OWO, owner);
+        USDTMock(address(new USDTMock())).transfer(adr, 0xd3c21bcecceda1000000);
+    }
+}
